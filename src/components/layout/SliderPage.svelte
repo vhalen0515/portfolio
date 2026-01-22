0d9ff428742b1e2a5ff7be68972186338e0c1f00
Trent Avilla <131209117+vhalen0515@users.noreply.github.com>
2026-01-22 05:51:32 -0800

    Fixed text error in slider

--- src/components/layout/SliderPage.svelte
+++ src/components/layout/SliderPage.svelte
@@ -228,7 +228,7 @@
                         >
                             <h2>{slide.title}</h2>
                         </a>
-                        <p>{slide.description}</p>g
+                        <p>{slide.description}</p>
                         <ul>
                             {#each slide.tools as tools}
                                 <li>{tools}</li>
